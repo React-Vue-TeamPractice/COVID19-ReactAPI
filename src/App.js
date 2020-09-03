39e50e4685c5b2b2341cd2d7de3c820094c56055
Ilya <ilyaphotoshop17@gmail.com>
2020-09-04 01:44:18 +0300

    [refactoring]

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 //import components
 import { CircularDeterminate } from './components/elements/loading';
 import { CovidHook } from './components/hooks/useCovidHook';
 import { Cards, CountryPicker, Chart} from './components'
 import styles from './components/style/index.css'
+import Axios from 'axios'
+import { API_URL } from './components/config/index'
 
-class App extends React.Component  {
+// class App extends React.Component  {
 
-  state = {
-      data: {}
-    }
+//   constructor(props) {
+//     super(props)
+//    this.state = {
+//       data: []
+//     }
+//   }
 
-  async componentDidMount() {
-    const fetchData = await CovidHook()
+//    componentDidMount() {
+//     Axios.get(API_URL)
+//       .then(res => this.setState({data: res.Ukraine}))
+//     console.log(this.data)
+//   }
+//     render () {
+//       const { data } = this.state.data
+//       console.log(this.data)
 
-    this.setState({data: fetchData})
-    console.log(fetchData)
-  }
+//         return (
+//           <div className='container'>
+//               COVID-19
+             
+//          </div>
+//       )
+//     }
+// }
 
-    render () {
-      const { data } = this.state
-        return (
-          <div className='container'>
-              COVID-19
-              <Cards
-                  data={data}
-              />
-              <CountryPicker />
-              <Chart />
-         </div>
-      )
-    }
+const App = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+      try {
+          Axios.get(API_URL)
+          .then(res => setData([res,data]))
+      } catch(e) {
+        console.log(e);
+      }
+  }, [])
+  console.log(data)
+  return (
+    data
+  )
 }
 
 export default App;
